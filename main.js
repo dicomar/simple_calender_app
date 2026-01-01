c3fef5980ad757504d6a0c03ff5fe7744da12387
dicomar <127745122+dicomar@users.noreply.github.com>
2026-01-01 16:32:35 +0300

    files have been updated

---
+++ main.js
@@ -0,0 +1,4 @@
+const p_Month = document.getElementById("prev_Month"),
+  n_Month = document.getElementById("next_Month"),
+  m_Name = document.getElementById("month_Name"),
+  y_Dsp = document.getElementById("year_Display");
