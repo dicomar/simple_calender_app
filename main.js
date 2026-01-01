ba0f7e714d5b2a7023f23cab8d85a33b8d569c11
dicomar <127745122+dicomar@users.noreply.github.com>
2026-01-01 16:54:17 +0300

    commiting changes

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 const p_Month = document.getElementById("prev_Month"),
-  n_Month = document.getElementById("next_Month"),
-  m_Name = document.getElementById("month_Name"),
-  y_Dsp = document.getElementById("year_Display");
+    n_Month = document.getElementById("next_Month"),
+    m_Name = document.getElementById("month_Name"),
+    y_Dsp = document.getElementById("year_Display");
+
+let y_No = 0,
+    m_No = 0,
+    cur_Date = new Date(), ls_Date = new Date(cur_Date.getFullYear(), cur_Date.getMonth+1, 0),
+    fs_Day = new Date(cur_Date.getFullYear(), cur_Date.getMonth, 1).getDay();
+
+
+
+y_Dsp.textContent = cur_Date.getFullYear();
+m_Name.textContent = new Date(cur_Date.getFullYear(), cur_Date.getMonth())
+    .toLocaleString(0, { month: "long" })
+    .toUpperCase();
