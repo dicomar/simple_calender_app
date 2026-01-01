607f26fbbc736887fa70e7d3acaee091bf0f202f
dicomar <127745122+dicomar@users.noreply.github.com>
2026-01-01 17:29:49 +0300

    changes committed

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 const p_Month = document.getElementById("prev_Month"),
-    n_Month = document.getElementById("next_Month"),
-    m_Name = document.getElementById("month_Name"),
-    y_Dsp = document.getElementById("year_Display");
+  n_Month = document.getElementById("next_Month"),
+  m_Name = document.getElementById("month_Name"),
+  y_Dsp = document.getElementById("year_Display"),
+  cal_Body = document.getElementById("calender_Body");
 
-let y_No = 0,
-    m_No = 0,
-    cur_Date = new Date(), ls_Date = new Date(cur_Date.getFullYear(), cur_Date.getMonth+1, 0),
-    fs_Day = new Date(cur_Date.getFullYear(), cur_Date.getMonth, 1).getDay();
+let cur_Date = new Date(),
+  y_No = cur_Date.getFullYear(),
+  m_No = cur_Date.getMonth(),
 
+  ls_Date = new Date(y_No, m_No+1, 0).getDate(),
+  fs_Day = new Date(cur_Date.getFullYear(), cur_Date.getMonth, 1).getDay();
 
+var generateCalender = (y, m) => {
+    cal_Body.innerHTML = ""
+  fs_Day = new Date(y, m + 1, 1).getDay();
+  ls_Date = new Date(y, m + 1, 0).getDate();
 
-y_Dsp.textContent = cur_Date.getFullYear();
-m_Name.textContent = new Date(cur_Date.getFullYear(), cur_Date.getMonth())
-    .toLocaleString(0, { month: "long" })
-    .toUpperCase();
+  for (let d = 1; d <= fs_Day; d++) {
+    cal_Body.innerHTML += `<div class="month-date dummy-date">${"null"}</div>`;
+  }
+
+  for (let d = 1; d <= ls_Date; d++) {
+    d===new Date().getDate() ? cal_Body.innerHTML += `<div class="month-date current-date">${d}</div>`:
+    cal_Body.innerHTML += `<div class="month-date">${d}</div>`
+  }
+ 
+  m_Name.textContent = new Date(y, m).toLocaleString(0, {month:'long'}).toUpperCase();
+   y_Dsp.textContent = new Date(y, m).toLocaleString(0, {year:'numeric'});
+};
+
+generateCalender(y_No, m_No);
+
+p_Month.onclick = ()=> {
+    m_No--
+    generateCalender(y_No, m_No);
+}
+
+n_Month.onclick = ()=> {
+    m_No++
+    generateCalender(y_No, m_No);
+}
